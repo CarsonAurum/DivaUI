5f404a53dda8bf2a78346b58a2105232109c115c
Carson R <53197646+CarsonAurum@users.noreply.github.com>
2022-06-03 10:28:01 -0500

    updates

--- Package.swift
+++ Package.swift
@@ -1,23 +1,18 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "DivaUI",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DivaUI",
             targets: ["DivaUI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/CarsonAurum/DivaLibrary.git", branch: "master")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DivaUI",
             dependencies: []),
